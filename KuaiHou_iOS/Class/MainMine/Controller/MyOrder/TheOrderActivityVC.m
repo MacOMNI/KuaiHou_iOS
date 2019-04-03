5fb1480011dd0b89a22a9e5160ae98cf6cddf944
郭子豪 <862124770@qq.com>
2019-04-03 15:14:52 +0800

    订单这一块ojbk

--- KuaiHou_iOS/Class/MainMine/Controller/MyOrder/TheOrderActivityVC.m
+++ KuaiHou_iOS/Class/MainMine/Controller/MyOrder/TheOrderActivityVC.m
@@ -7,6 +7,7 @@
 //
 
 #import "TheOrderActivityVC.h"
+#import "SendDynamicsHeadCell.h"
 
 @interface TheOrderActivityVC ()<UICollectionViewDelegate, UICollectionViewDataSource>
 @property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
@@ -19,9 +20,45 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     self.title = @"此单活动";
+    [self fixUI];
 }
 
+-(void)fixUI{
+    self.collectionView.delegate = self;
+    self.collectionView.dataSource = self;
+    UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc] init];
+    layout.minimumLineSpacing = 15;
+    layout.minimumInteritemSpacing = 15;
+    [self.collectionView setCollectionViewLayout:layout];
+    
+    NSBundle *bund=[NSBundle bundleForClass:[self class]];
+    [self.collectionView registerNib:[UINib nibWithNibName:NSStringFromClass([SendDynamicsHeadCell class]) bundle:bund] forCellWithReuseIdentifier:NSStringFromClass([SendDynamicsHeadCell class])];
+}
 
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+    return 1;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 6;
+}
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    CGFloat width = (kScreenSizeWidth- 60) / 3;
+    return CGSizeMake(width, width);
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    SendDynamicsHeadCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:NSStringFromClass([SendDynamicsHeadCell class]) forIndexPath:indexPath];
+    
+    cell.delBtn.hidden = YES;
+
+    return cell;
+}
 
 /*
 #pragma mark - Navigation
